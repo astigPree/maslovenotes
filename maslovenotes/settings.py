accc048fab8ab7b78e150233c97a29b66135f29f
MakieTech <121760681+astigPree@users.noreply.github.com>
2026-02-09 22:52:33 +0800

    setup settings for postgres

--- maslovenotes/settings.py
+++ maslovenotes/settings.py
@@ -10,11 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
+print("DEBUG:", os.getenv('DEBUG'))
+print("WORKING_AREA:", os.getenv('WORKING_AREA'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -42,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,14 +84,44 @@ WSGI_APPLICATION = 'maslovenotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('WORKING_AREA') == 'LOCAL': 
+    DATABASES = { 
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': BASE_DIR / 'db.sqlite3',
+        # }, 
+        'sqlite': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }, 
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRESS_NAME' , 'postgres'),
+            'USER': os.getenv('POSTGRESS_USER' , 'postgres'),
+            'PASSWORD': os.getenv('POSTGRESS_PASSWORD' , 'postgres'),
+            'HOST': os.getenv('POSTGRESS_HOST', 'localhost'),
+            'PORT': os.getenv('POSTGRESS_PORT', '5432'),
+        }
+
     }
-}
-
-
+elif os.getenv('WORKING_AREA') == 'PYTHONANYWHERE':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('PYTHONANYWHERE_MYSQL_NAME'),   # full name from PythonAnywhere
+            'USER': os.getenv('PYTHONANYWHERE_MYSQL_USERNAME'),
+            'PASSWORD': os.getenv('PYTHONANYWHERE_MYSQL_PASSWORD'),
+            'HOST': os.getenv('PYTHONANYWHERE_MYSQL_HOST'),
+            # 'PORT': os.getenv('PYTHONANYWHERE_MYSQL_PORT', '3306')
+        }
+    } 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }  
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
